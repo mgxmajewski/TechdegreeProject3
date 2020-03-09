3ea1c5e172f68ad1982b143f5ef04181be04abbb
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2020-03-09 17:16:16 +0100

    adds "Focus" and "Other Job Input"

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// console.log('test'); - checking if file is conected
+const name = document.getElementById("name");
+name.focus();
+const jobRole = document.getElementById("other-title");
+jobRole.style.display = 'none';
