4f5edd4faf121d6634207764c091ef6b616fe4ea
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2020-03-10 11:29:40 +0100

    fixed T-shit Color and Theme droplists functionality

--- js/script.js
+++ js/script.js
@@ -4,49 +4,57 @@ name.focus();
 const jobRole = document.getElementById("other-title");
 jobRole.style.display = 'none';
 
+
+
+
+// Declaring variables for T-Shirt Info Dropdown scripti
 const designList = document.getElementById("design");
 const design = designList.getElementsByTagName('option');
-
-console.log(design);
 const color = document.getElementById("color").children;
-console.log(color);
 const colorDiv = document.getElementById("colors-js-puns");
-
-
-// console.log(design[0]);
-// console.log(design[1]);
-// console.log(design[2]);
-
-if (design[0]) {
-   colorDiv.style.display ="none";
-}
-
-designList.addEventListener('input', (e) => {
+const colorOptions = document.querySelectorAll('#color option')
+// Here adding to 'Color options' - "Please select a T-shirt theme"
+const colorParentNode = document.getElementById("color");
+const selectTheme = document.createElement("option");
+selectTheme.innerHTML = "Please select a T-shirt theme";
+colorParentNode.prepend(selectTheme);
+selectTheme.indexOf = 0;
+color[0].selected = true;
+// Here hidding all 'Color options'
+const hideColors = () => {
+for (let i = 1; i < color.length; i++) {
+   color[i].style.display="none";
+   }
+};
+hideColors();
+// Here adding to 'Color options' - "Please select a T-shirt theme"
+designList.addEventListener('change', (e) => {
+   
    design[0].style.display="none";
    colorDiv.style.display ="";
    const clicked = e.target.value;
-   console.log(clicked);
-   // const choice = e.target;
-   // console.log(choice);
+   color[0].style.display="";
   for (let i = 0; i < design.length ; i++){
      if (clicked == 'js puns') {
-      color[0].style.display="";
+      color[0].style.display="none";
+      color[1].selected = true;
       color[1].style.display="";
       color[2].style.display="";
-      color[3].style.display="none";
+      color[3].style.display="";
       color[4].style.display="none";
       color[5].style.display="none";
-      // console.log(design[i].textContent);
-      // console.log('1st loop');
-     } else {
+      color[6].style.display="none";
+      console.log('1st loop');
+     } else if (clicked === 'heart js'){
       color[0].style.display="none";
+      color[4].selected = true;
       color[1].style.display="none";
       color[2].style.display="none";
-      color[3].style.display="";
+      color[3].style.display="none";
       color[4].style.display="";
       color[5].style.display="";
-      // console.log(design[i].textContent);
-      // console.log('2nd loop');
+      color[6].style.display="";
+      console.log('2nd loop');
      }
   }
 });
