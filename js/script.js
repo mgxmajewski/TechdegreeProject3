716ceda74934144d4588ac434ce607d78d14f524
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2020-03-10 08:44:21 +0100

    makes "T-Shir Color Chice" work

--- js/script.js
+++ js/script.js
@@ -5,31 +5,48 @@ const jobRole = document.getElementById("other-title");
 jobRole.style.display = 'none';
 
 const designList = document.getElementById("design");
-const design = document.getElementById("design").children;
+const design = designList.getElementsByTagName('option');
+
 console.log(design);
 const color = document.getElementById("color").children;
 console.log(color);
 const colorDiv = document.getElementById("colors-js-puns");
 
 
+// console.log(design[0]);
+// console.log(design[1]);
+// console.log(design[2]);
+
 if (design[0]) {
    colorDiv.style.display ="none";
 }
 
 designList.addEventListener('input', (e) => {
-   
    design[0].style.display="none";
    colorDiv.style.display ="";
-   const choice = e.target;
-  for (let i = 1; i < design.length -1; i++){
-     if (design[1]){
-        color[0].style.display="none";
-        color[1].style.display="none";
-        color[2].style.display="none";
-     } else {
+   const clicked = e.target.value;
+   console.log(clicked);
+   // const choice = e.target;
+   // console.log(choice);
+  for (let i = 0; i < design.length ; i++){
+     if (clicked == 'js puns') {
+      color[0].style.display="";
+      color[1].style.display="";
+      color[2].style.display="";
       color[3].style.display="none";
       color[4].style.display="none";
       color[5].style.display="none";
+      // console.log(design[i].textContent);
+      // console.log('1st loop');
+     } else {
+      color[0].style.display="none";
+      color[1].style.display="none";
+      color[2].style.display="none";
+      color[3].style.display="";
+      color[4].style.display="";
+      color[5].style.display="";
+      // console.log(design[i].textContent);
+      // console.log('2nd loop');
      }
   }
 });
